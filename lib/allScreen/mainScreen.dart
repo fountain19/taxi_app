973dd972af52878bb3f3c477474f9ca5e24dd67c
fountain19 <fountainyasser@yahoo.com>
2020-12-31 02:02:52 +0300

    31/12/2020

--- lib/allScreen/mainScreen.dart
+++ lib/allScreen/mainScreen.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:taxi_app/allWidgets/divider.dart';
 
 class MainScreen extends StatefulWidget {
   static const String idScreen='mainScreen';
@@ -11,6 +12,7 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
 
+  GlobalKey<ScaffoldState> scaffoldkey= GlobalKey<ScaffoldState>();
   Completer<GoogleMapController> _controllerGoogleMap = Completer();
   GoogleMapController newGoogleMapController;
 
@@ -22,23 +24,200 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldkey,
       appBar: AppBar(
+        iconTheme: IconThemeData(
+          color: Colors.black
+        ),
         backgroundColor: Colors.yellow,
         title: Text('Main Screen',style: TextStyle(color: Colors.black,
         fontWeight: FontWeight.bold,
         ),),
         centerTitle: true,
       ),
-      body:GoogleMap(
-        mapType: MapType.normal,
-        myLocationButtonEnabled: true,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller)
-        {
-          _controllerGoogleMap.complete(controller);
-          newGoogleMapController=controller;
-        },
+      drawer: Container(
+        width: 255,
+        color: Colors.white,
+        child: Drawer(
+          child: ListView(
+            children: [
+              Container(
+                height: 165,
+                child: DrawerHeader(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                  ),
+                  child: Row(
+                    children: [
+                      Image.asset('images/user_icon.png',height: 65,width: 65,),
+                      SizedBox(width:16.0,),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text('Profile Name',style: TextStyle(
+                              fontSize:16.0,fontFamily: 'bolt-regular'),),
+                          SizedBox(height: 6.0,),
+                          Text('Visit profile'),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              DividerWidget(),
+              SizedBox(height: 12.0,),
+              // drawer body controller
+              ListTile(
+                leading: Icon(Icons.history),
+                title: Text('History',style: TextStyle(fontSize: 15),),
+              ),
+              ListTile(
+                leading: Icon(Icons.person),
+                title: Text('Visit profile',style: TextStyle(fontSize: 15),),
+              ),
+              ListTile(
+                leading: Icon(Icons.info),
+                title: Text('About',style: TextStyle(fontSize: 15),),
+              ),
+            ],
+          ),
+        ),
       ),
-    );
+      body:Stack(
+    children: [
+            GoogleMap(
+            mapType: MapType.normal,
+            myLocationButtonEnabled: true,
+            initialCameraPosition: _kGooglePlex,
+            onMapCreated: (GoogleMapController controller)
+            {
+            _controllerGoogleMap.complete(controller);
+            newGoogleMapController=controller;
+            },
+            ),
+    // HamburgerButton for Drawer
+        Positioned(
+          top: 45.0,left: 22.0,
+          child: GestureDetector(
+            onTap: (){
+            scaffoldkey.currentState.openDrawer();
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(22.0),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.black,
+                        blurRadius: 6.0,
+                        spreadRadius: 0.5,
+                        offset: Offset(0.7,0.7)
+                    )
+                  ]
+              ),
+              child: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Icon(Icons.menu,color: Colors.black,),
+                radius: 20.0,
+              ),
+            ),
+          ),
+        ),
+    Positioned(
+    left: 0.0,right: 0.0,bottom: 0.0,
+    child: Container(
+       height: 320.0,
+    decoration: BoxDecoration(
+       color: Colors.white,
+    borderRadius: BorderRadius.only(
+      topLeft: Radius.circular(18.0),topRight: Radius.circular(18.0),
+    ),
+    boxShadow: [
+      BoxShadow(
+        color: Colors.black,
+        blurRadius: 16.0,
+        spreadRadius: 0.5,
+        offset: Offset(0.7,0.7)
+      )
+    ]
+    ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0,vertical: 18.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 6.0,),
+            Text('Hi there ',style:TextStyle(fontSize: 12.0),),
+            Text('Where to ?',style: TextStyle(fontSize:20.0,fontFamily: 'bolt-regular'),),
+            SizedBox(height: 20.0,),
+            Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(5.0),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.black54,
+                        blurRadius: 6.0,
+                        spreadRadius: 0.5,
+                        offset: Offset(0.7,0.7)
+                    )
+                  ]
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Row(
+                  children: [
+                    Icon(Icons.search,color: Colors.blueAccent,),
+                    SizedBox(width: 10.0,),
+                    Text('Search drop off')
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 24.0,),
+            Row(
+              children: [
+                Icon(Icons.home,color: Colors.grey,),
+                SizedBox(width: 12.0,),
+                Column(
+                  children: [
+                    Text('Add home'),
+                    SizedBox(height: 4.0,),
+                    Text('Your living home address',
+                      style: TextStyle(
+                          fontSize:12.0,color: Colors.black54),),
+                  ],
+                )
+
+              ],
+            ),
+            SizedBox(height: 10.0,),
+            DividerWidget(),
+            SizedBox(height: 16.0,),
+            Row(
+              children: [
+                Icon(Icons.work,color: Colors.grey,),
+                SizedBox(width: 12.0,),
+                Column(
+                  children: [
+                    Text('Add work'),
+                    SizedBox(height: 4.0,),
+                    Text('Your office address',
+                      style: TextStyle(
+                          fontSize:12.0,color: Colors.black54),),
+                  ],
+                )
+
+              ],
+            )
+          ],
+        ),
+      ),
+    ),
+    )
+
+    ],
+
+    ));
   }
 }
